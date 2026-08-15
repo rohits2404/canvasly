b725b8802aaeb0daecc95e87cc57c945036b78d8
Rohit Sharma <rohits8731@gmail.com>
2026-08-15 17:11:18 +0000

    21: Editor File Upload

--- src/features/editor/components/image-sidebar.tsx
+++ src/features/editor/components/image-sidebar.tsx
@@ -7,6 +7,7 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import Image from "next/image";
 import Link from "next/link";
 import { ToolSidebarClose } from "./tool-sidebar-close";
+import { UploadButton } from "@/lib/uploadthing";
 
 interface ImageSidebarProps {
     editor: Editor | undefined;
@@ -36,6 +37,21 @@ export const ImageSidebar = ({
                 title="Images"
                 description="Add Images To Your Canvas"
             />
+            <div className="p-4 border-b">
+                <UploadButton
+                appearance={{
+                    button: "w-full text-sm font-medium",
+                    allowedContent: "hidden"
+                }}
+                content={{
+                    button: "Upload Image"
+                }}
+                endpoint="imageUploader"
+                onClientUploadComplete={(res) => {
+                    editor?.addImage(res[0].ufsUrl);
+                }}
+                />
+            </div>
             {isLoading && (
                 <div className="flex items-center justify-center flex-1">
                     <Loader className="size-4 text-muted-foreground animate-spin" />
